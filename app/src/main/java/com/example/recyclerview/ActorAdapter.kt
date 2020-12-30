5a617458771c610eb06904acd5d5e2ef77b0902c
a@gmail.com <a@gmail.com>
2020-12-30 12:50:15 +0545

    Actor Adapter Created

---
+++ app/src/main/java/com/example/recyclerview/ActorAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerview
+
+import android.content.Context
+import com.example.recyclerview.adapter.Actors
+
+class ActorAdapter(
+    val lstActors : ArrayList<Actors>,
+    val context : Context
+    ) : RecyclerView.Adapter<ActorAdapter.ActorViewHolder>() {
+    class ActorViewHolder(view : View) : Recy
+}
---
+++ app/src/main/java/com/example/recyclerview/adapter/Actors.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerview.adapter
+
+data class Actors(
+    val actorId : Int? = null,
+    val actorName : String ? = null,
+    val actorImage : String? = null,
+    val actorAddress : String? = null,
+    val actorSalary : Float? = null
+)
